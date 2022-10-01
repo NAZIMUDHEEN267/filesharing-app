598f9dc3507641da0f0a5947ffbe51b6baf28fd6
nazimudheen267 <nazimudheen267@gmail.com>
2022-10-02 03:00:30 +0530

    added some styles

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.navBar}>
+        <Text style={styles.headline}>File sharing</Text>
+      </View>
+      <TouchableOpacity onPress={() => alert("Hello World!!")} style={styles.btn}>
+        <Text style={styles.btnText}>Click Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  navBar: {
+    backgroundColor: "red",
+    padding: 15,
+    borderWidth: 6,
+    borderColor: "red",
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 30
   },
-});
+  headline: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "600"
+  },
+  btn: {
+    backgroundColor: "blue",
+    padding: 20,
+    marginTop: 20
+  },
+  btnText: {
+    backgroundColor: "red", 
+    color: "white", 
+    fontSize: 20
+  }
+})
