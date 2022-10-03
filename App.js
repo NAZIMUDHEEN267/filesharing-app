4ad450a9ff560a08c9e3f3157133b11f9b9994eb
nazimudheen267 <nazimudheen267@gmail.com>
2022-10-03 05:59:02 +0530

    share option added

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
-import { Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView, Platform } from 'react-native';
 import Main from './Components/Main';
-// import fileImag from "./assets/file-sharing.jpg";
+
 
 export default function App() {
   const [clr, setClr] = useState("red");
@@ -12,9 +11,11 @@ export default function App() {
     <View style={{ backgroundColor: "rgba(0, 0, 0, .2)", height: "100%" }}>
       <StatusBar style='auto'></StatusBar>
       <View style={styles.navBar}>
-        <Text style={[styles.headline, {backgroundColor: clr} ]}>File sharing</Text>
+        <Text style={[styles.headline, { backgroundColor: clr }]}>File sharing</Text>
       </View>
-      <Main btnStyle={styles.btn} />
+      <View style={styles.container}>
+        <Main btnStyle={styles.btn} />
+      </View>
     </View>
   );
 }
@@ -34,6 +35,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "white",
     fontWeight: "600"
+  },
+  container: {
+    flex: .8,
+    alignItems: "center",
+    justifyContent: 'center',
   },
   btn: {
     paddingLeft: "35%",
--- Components/Main.js
+++ Components/Main.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { Button } from "react-native-web";
+import * as imageShare from "expo-sharing";
+import * as imageManipulator from "expo-image-manipulator";
 
 function Main({ btnStyle }) {
 
     const sample = "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg";
-    const [img, setImg] = useState(sample);
+    const [img, setImg] = useState(null);
 
     let imagePickerAsync = async () => {
         let pickerResult = await ImagePicker.launchImageLibraryAsync();
@@ -15,24 +16,49 @@ function Main({ btnStyle }) {
         }
     }
 
-    return (
-        <View style={styles.centerBox}>
-            <View style={styles.image}>
-                <Image source={{ uri: img }} style={{ height: '100%', width: "100%" }} />
+    let openShareDialogueAsync = async () => {
+        if(Platform.OS === "web") {
+            alert("This functionality not available in web, Sorry...")
+        }
+
+        imageManipulator.manipulateAsync(img).then(async imgTemp => {
+            await imageShare.shareAsync(imgTemp.uri);
+        }).catch(err => {
+            throw new Error(err); 
+        })
+    }
+
+    if(img) {
+        return (
+            <View style={styles.centerBox}>
+                <View style={styles.image}>
+                    <Image source={{ uri: img }} style={{ height: '100%', width: "100%" }} />
+                </View>
+                <TouchableOpacity onPress={openShareDialogueAsync} style={btnStyle}>
+                    <Text>Tap to Share</Text>
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={imagePickerAsync} style={btnStyle}>
-                <Text>Click Me</Text>
-            </TouchableOpacity>
-        </View>
-    )
+        )
+    } else {
+        return (
+            <View style={styles.centerBox}>
+                <View style={styles.image}>
+                    <Image source={{ uri: img }} style={{ height: '100%', width: "100%" }} />
+                </View>
+                <TouchableOpacity onPress={imagePickerAsync} style={btnStyle}>
+                    <Text>Click Me</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
 }
 
 const styles = {
     centerBox: {
         width: 200,
         height: 200,
-        backgroundColor: "blue",
-        margin: "25%"
+        backgroundColor: "blue"
     },
     image: {
         padding: 10
